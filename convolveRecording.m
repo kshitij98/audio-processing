b5c40609c98a7d5f1a52e4972fb57c7fb39faf61
kshitij98 <kgupta119@gmail.com>
2018-01-18 11:32:38 +0530

    Initial Commit

---
+++ convolveRecording.m
@@ -0,0 +1,26 @@
+Fs = 44100
+recorder = audiorecorder(Fs, 8, 1);
+
+disp('start');
+recordblocking(recorder, 2);
+disp('stop')
+
+% play(recorder);
+y = getaudiodata(recorder)
+
+% [y, Fs] = audioread('./audio-processing/song.wav');
+[church, Fs] = audioread('./audio-processing/ir_church.wav');
+
+samplingRates = [24000 16000 8000 4000];
+samplingRatesRatio = Fs ./ samplingRates
+
+H24 = y(round((1:(size(y, 1) / samplingRatesRatio(1))) .* samplingRatesRatio(1)));
+H16 = y(round((1:(size(y, 1) / samplingRatesRatio(2))) .* samplingRatesRatio(2)));
+H8 = y(round((1:(size(y, 1) / samplingRatesRatio(3))) .* samplingRatesRatio(3)));
+H4 = y(round((1:(size(y, 1) / samplingRatesRatio(4))) .* samplingRatesRatio(4)));
+
+% sound(H24, samplingRates(1))
+% sound(H16, samplingRates(2))
+% sound(H8, samplingRates(3))
+sound(H4, samplingRates(4))
+
