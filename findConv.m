ad7dbfca50ef56a616ac0e2c6f1acf418eed0103
kshitij98 <kgupta119@gmail.com>
2018-01-18 20:07:28 +0530

    Added findConv.m

---
+++ findConv.m
@@ -0,0 +1,21 @@
+Input = [12 20 3 10 22 19 23 16 0 21 23 16 18]
+Output = [75 52 33 97 251 211 63 65]
+
+size(Input)
+size(Output)
+filterS = size(Input) - size(Output);
+filterS = filterS + repmat(1, size(filterS))
+
+A = zeros(filterS(2));
+
+for i = 1:size(Input, 2) - filterS(2) + 1
+	A(i, :) = Input(i:i+filterS(2)-1);
+end
+
+% size(pinv(A))
+% size(Output)
+
+Filter = pinv(A) * (Output')
+Filter = fliplr(Filter')
+
+Result = conv(Input, Filter)
